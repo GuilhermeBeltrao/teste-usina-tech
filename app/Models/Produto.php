8bbaf090acc5a4f9240f293ea1428b8cd799bbce
GuilhermeBeltrao <guilhermebeltraodev@protonmail.com>
2021-06-15 19:38:40 -0300

    model de categorias + view de produto

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -8,8 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Produto extends Model
 {
     use HasFactory;
+    
+    protected $fillable = [
+        'nome',
+        'marca_id',
+        'categoria_id'
+    ];
 
     public function marcas() {
         return $this->belongsTo(Marca::class);
+    }
+    
+    public function categorias() {
+        return $this->belongsTo(Categoria::class);
     }
 }
---
+++ resources/views/produtos/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                    @if(auth()->user()->is_admin == 1)
+                    <div class='panel-heading'>sem permissao para acessar a pagina</div>
+                    @else
+                    <div class=”panel-heading”>Normal User</div>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
